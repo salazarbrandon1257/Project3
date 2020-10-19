4b8f57746d69e34f7c4a0cb9d9c5e2f9e43106dd
salazarbrandon1257 <salazarbrandon1257@gmail.com>
2020-10-19 16:37:30 -0500

    Starting on wordPuzzle

--- WordPuzzle.java
+++ WordPuzzle.java
@@ -1,16 +1,27 @@
 import java.io.*; 
+
 public class WordPuzzle {
     public static void main(String[] args)throws Exception { 
     File file = new File("C:\\Users\\Brandon Salazar\\Desktop\\dictionary.txt"); 
     BufferedReader br = new BufferedReader(new FileReader(file)); 
     String st; 
     int tableSize = 0;
-    while ((st = br.readLine()) != null && tableSize < 10){
+    int maxLength = 0;
+    String maxWord = "";
+    while ((st = br.readLine()) != null){
         tableSize++; 
-        System.out.println(st);  
+        // System.out.println(st); 
+        if (st.length() > maxLength){
+          maxLength = st.length();
+          maxWord = st;
+        }
     } 
     // table size is 109616
     System.out.print("Tablesize: ");
     System.out.println(tableSize);
+    System.out.print("maxLength: ");
+    System.out.println(maxLength);
+    System.out.print("maxWord: ");
+    System.out.println(maxWord);
   } 
 }
